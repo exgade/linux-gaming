0e7dc4d98750d8f78fb953bea86fc6cf9eaa38d8
Webdesign Höhne <info@webdesign-hoehne.de>
2020-03-07 22:20:16 +0100

    Artix Linux Support

--- autoinstall.sh
+++ autoinstall.sh
@@ -2,12 +2,20 @@
 workdir="`dirname $0`"
 distrodetect=""
 distroinstaller=""
-if [ "`cat /etc/os-release | grep 'ID=manjaro' | wc -l`" = "1" ] ; then
-	distrodetect="Manjaro"
-	distroinstaller="manjaro"
-elif [ "`cat /etc/os-release | grep 'ID=arch' | wc -l`" = "1" ] ; then
-	distrodetect="Arch Linux"
-	distroinstaller="arch"
+if [ -f /etc/os-release ] ; then
+	if [ "`cat /etc/os-release | grep 'ID=manjaro' | wc -l`" = "1" ] ; then
+		distrodetect="Manjaro"
+		distroinstaller="manjaro"
+	elif [ "`cat /etc/os-release | grep 'ID=arch' | wc -l`" = "1" ] ; then
+		distrodetect="Arch Linux"
+		distroinstaller="arch"
+	fi
+else
+	distrochk="`distro | grep Name: | sed "s/Name: //" -`"
+	if [ "${distrochk}" = "Artix Linux" ] ; then
+		distrodetect="Artix Linux"
+		distroinstaller="arch"	
+	fi
 fi
 
 if [ "${distrodetect}" = "" ] ; then
