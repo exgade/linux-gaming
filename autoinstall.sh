685045912bd945754f537279f58c1e5aeb47495e
Webdesign Höhne <info@webdesign-hoehne.de>
2020-03-13 11:20:00 +0100

    modified autoinstaller to handle base ubuntu, elementary& linux mint

--- autoinstall.sh
+++ autoinstall.sh
@@ -12,6 +12,21 @@ if [ -f /etc/os-release ] ; then
 	elif [ "`grep 'ID=debian' /etc/os-release | wc -l`" = "1" ] ; then
 		distrodetect="Debian"
 		distroinstaller="debian"
+	elif [ "`grep 'ID=ubuntu' /etc/os-release | wc -l`" = "1" ] ; then
+		distrodetect="Ubuntu"
+		distroinstaller="ubuntu"
+	elif [ "`grep 'ID_LIKE=ubuntu' /etc/os-release | wc -l`" = "1" ] ; then
+		if [ "`grep 'ID=elementary' /etc/os-release | wc -l`" = "1" ] ; then
+			distrodetect="Elementary"
+		elif [ "`grep 'ID=elementary' /etc/os-release | wc -l`" = "1" ] ; then
+			distrodetect="Linux Mint"
+		else
+			distrodetect="Unknown Ubuntu"
+			echo "WARNING: this ubuntu distribution is unknown, you might face a broken system if you continue."
+			echo "To abort press CTRL+C, continue anyway with ENTER"
+			read
+		fi
+		distroinstaller="ubuntu"
 	fi
 elif [ -f /usr/lib/os-release ] ; then
 	if [ "`grep 'ID=artix' /usr/lib/os-release | wc -l`" = "1" ] ; then
