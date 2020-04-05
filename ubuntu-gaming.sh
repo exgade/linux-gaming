afae4162101af9916a8e4eac751df815f6af19c1
Andreas <info@webdesign-hoehne.de>
2020-04-05 11:32:21 +0200

    added recommended packages for ea games/ origin

--- ubuntu-gaming.sh
+++ ubuntu-gaming.sh
@@ -110,7 +110,7 @@ fi
 
 # install common required libraries
 echo "### installing libraries that are need for many games"
-apt install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libsqlite3-0:i386 
+apt install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libsqlite3-0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386
 
 # software installation flathub
 if [[ "${teamspeak_install}" = "true" || "${discord_install}" = "true" ]] ; then
