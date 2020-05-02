0e6f36d76cd1da67a0a2402109dc55595b43c966
Webdesign Höhne <info@webdesign-hoehne.de>
2020-05-02 17:47:10 +0200

    optimized code of ui-install.sh and allow arguments on autoinstall.sh

--- ui-install.sh
+++ ui-install.sh
@@ -6,10 +6,11 @@ option_noconfirm="false"
 installer_arguments=""
 for arg in "$@" ; do
 	if [[ "$arg" = "--force" || "$arg" = "-f" ]] ; then
-		option_noconfirm="true"
+		installer_arguments="${installer_arguments} --force"
 	elif [[ "$arg" = "--help" || "$arg" = "-h" ]] ; then
 		echo "usage: ./ui-install.sh [OPTIONS]"
 		echo "--force - no questions while installing / uninstalling packages - this might break your system"
+
 		exit
 	fi
 done
@@ -37,6 +38,7 @@ function askQuestion {
 		answer="false"
 	fi
 }
+
 #if [ "${option_noconfirm}" = "false" ] ; then
 #	default="n"
 #	question="Install all forced? This might damage your system"
@@ -45,10 +47,6 @@ function askQuestion {
 #		option_noconfirm="true"
 #	fi
 #fi
-
-if [ "$option_noconfirm" = "true" ] ; then
-	installer_arguments="--force"
-fi
 
 default="y"
 question="Install Lutris?"
@@ -92,47 +90,4 @@ if [ "${answer}" != "true" ] ; then
 	installer_arguments="${installer_arguments} nodiscord"
 fi
 
-echo $installer_arguments
-
-if [ -f /etc/os-release ] ; then
-	source /etc/os-release
-	if [ "${ID}" = "manjaro" ] ; then
-		distrodetect="Manjaro"
-		distroinstaller="manjaro"
-	elif [ "${ID}" = "arch" ] ; then
-		distrodetect="Arch Linux"
-		distroinstaller="arch"
-	elif [ "${ID}" = "debian" ] ; then
-		distrodetect="Debian"
-		distroinstaller="debian"
-	elif [ "${ID}" = "ubuntu" ] ; then
-		distrodetect="Ubuntu"
-		distroinstaller="ubuntu"
-	elif [ "${ID_LIKE}" = "ubuntu" ] ; then
-		if [ "${ID}" = "elementary" ] ; then
-			distrodetect="Elementary"
-		elif [ "${ID}" = "linuxmint" ] ; then
-			distrodetect="Linux Mint"
-		else
-			distrodetect="Unknown Ubuntu"
-			echo "WARNING: this ubuntu distribution is unknown, you might face a broken system if you continue."
-			echo "To abort press CTRL+C, continue anyway with ENTER"
-			read
-		fi
-		distroinstaller="ubuntu"
-	fi
-elif [ -f /usr/lib/os-release ] ; then
-	source /usr/lib/os-release
-	if [ "${ID}" = "artix" ] ; then
-		distrodetect="Artix Linux"
-		distroinstaller="arch"	
-	fi
-fi
-
-if [ "${distrodetect}" = "" ] ; then
-	echo "No Distribution detected"
-else
-	echo "Detected Distribution ${distrodetect}, using installer ${distroinstaller}-gaming.sh"
-	cd "${workdir}" && ./${distroinstaller}-gaming.sh $installer_arguments
-fi
-
+cd "${workdir}" && ./autoinstall.sh $installer_arguments
