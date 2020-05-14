b140391d3a31072134359a09e105effa2f514768
Webdesign Höhne <info@webdesign-hoehne.de>
2020-05-14 11:17:50 +0200

    update versioncheck to 5.8-GE-2-MF

--- build-helpers/ge-notify-update.sh
+++ build-helpers/ge-notify-update.sh
@@ -3,9 +3,11 @@
 #following can be added to your crontab to get notifications every hour, when there is a new ge-proton version (without # at start)
 #5 * * * * /path-to/linux-gaming/build-helpers/ge-notify-update.sh
 
+# install / activate notification daemon on arch/manjaro:
+# sudo pacman -S notification-daemon && sudo systemctl start 
 scriptdir="$(dirname "$0")"
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${UID}/bus 
-if [ "Latest Release: 5.4-GE-3" = "$("$scriptdir"/ge-proton-shaupdate.sh check)" ] ; then
+if [ "Latest Release: 5.8-GE-2-MF" = "$("$scriptdir"/ge-proton-shaupdate.sh check)" ] ; then
 	echo "Version unchanged"
 else
 	echo "Version changed"
