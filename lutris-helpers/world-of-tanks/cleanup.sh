e97b2711503beaa1294a8af034f492cc8422c6a4
Webdesign Höhne <info@webdesign-hoehne.de>
2020-05-29 02:33:55 +0200

    moved promod files and added cleanup script for caches/replays

---
+++ lutris-helpers/world-of-tanks/cleanup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Installer to get oldskools promod working and updated in world of tanks installed by lutris
+lutrisPath="$(grep game_path "${HOME}/.config/lutris/system.yml" | sed "s/\s\sgame_path: //" -)/"
+wotprefix="${lutrisPath}world-of-tanks/"
+wotfolder="${wotprefix}drive_c"
+
+echo "WARNING: This will clear all heavyweight Cached Files, press Enter to continue or STRG+C to abort"
+read
+
+rm -Rdf "${wotfolder}"/users/[a-zA-Z-]*/Application\ Data/Wargaming.net/WorldOfTanks/web_cache/*
+rm -Rdf "${wotfolder}"/users/[a-zA-Z-]*/Application\ Data/Wargaming.net/WorldOfTanks/profile/cef_cache
+
+echo "WARNING: Now all replays will be deleted, if you continue with ENTER"
+read
+
+rm "$wotfolder"/Games/World_of_Tanks_EU/replays/*
+
+echo "deletion complete"
