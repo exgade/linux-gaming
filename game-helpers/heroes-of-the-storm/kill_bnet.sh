e68c63a4e654985c993af5ba83e8a2575fafa197
Webdesign Höhne <info@webdesign-hoehne.de>
2020-07-27 20:05:47 +0200

    improved script to kill bnet/hots

--- game-helpers/heroes-of-the-storm/kill_bnet.sh
+++ game-helpers/heroes-of-the-storm/kill_bnet.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-killall Agent.exe
+killedprocess="false"
+if [ "$(killall HeroesOfTheStorm_x64.exe 2>&1)" == "" ] ; then
+	killedprocess="true"
+fi
+if [ "$(killall Battle.net.exe 2>&1)" == "" ] ; then
+	killedprocess="true"
+fi
+if [ "$(killall Agent.exe 2>&1)" == "" ] ; then
+	killedprocess="true"
+fi
+if [ "$killedprocess" = "true" ] ; then
+	echo "Prozesse erfolgreich beendet"
+else
+	echo "No need to kill any processes"
+fi
