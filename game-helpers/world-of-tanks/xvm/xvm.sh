cf628dcd076f0d7620cb5ba8a041142670a2b899
Webdesign Höhne <info@webdesign-hoehne.de>
2020-12-23 17:41:16 +0100

    remove code, that's not needed

--- game-helpers/world-of-tanks/xvm/xvm.sh
+++ game-helpers/world-of-tanks/xvm/xvm.sh
@@ -12,26 +12,6 @@ prefix_name="world-of-tanks"
 #prefix_name="world-of-warships"
 wotprefix="${lutrisPath}${prefix_name}/"
 wotfolder="${wotprefix}drive_c"
-wineversion="$(grep -h -m1 "version:" "${HOME}/.config/lutris/games/${prefix_name}*.yml" | head -n1 | sed "s/\s\sversion: //g" -)"
-systemwine="false"
-#systemwine="true"
-if [[ "$wineversion" = "" || "$(echo "${wineversion}" | sed "s/lutris-[0-9]\.[0-9]\(\|-[0-9]\(\|[0-9]\)\)-x86_64//g" -)" != "" ]] ; then
-	echo "problem detecting wine version"
-	wineversion="lutris-5.7-11-x86_64"
-fi
-winefolder="${HOME}/.local/share/lutris/runners/wine/${wineversion}/"
-if [ "${systemwine}" = "true" ] ; then
-	wineexec="wine"
-elif [ -f "${winefolder}dist/bin/wine" ] ; then
-	# define wine executable for proton wine
-	wineexec="${winefolder}dist/bin/wine"
-elif [ -f "${winefolder}bin/wine" ] ; then
-	# define wine executable for lutris wine
-	wineexec="${winefolder}bin/wine"
-else
-	echo "unknown wine folder - please select an lutris or steam wine folder"
-	exit
-fi
 
 if [ ! -d "${wotfolder}" ] ; then
 	wotfolder="$(grep -h -m1 prefix "${HOME}/.config/lutris/games/world-of-tanks*.yml" | head -n1 | sed "s/\s\sprefix: //" -)"
