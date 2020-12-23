0e968fbd03628293cf4615f5cf6ec07cce8c37cb
Webdesign Höhne <info@webdesign-hoehne.de>
2020-12-23 11:48:43 +0100

    update install helpers to allow easier use of world-of-warships prefix

--- game-helpers/world-of-tanks/promod/install_promod.sh
+++ game-helpers/world-of-tanks/promod/install_promod.sh
@@ -8,15 +8,21 @@ if [ ! -d "${lutrisPath}" ] ; then
 	echo "Problem detecting Lutris Path, aborting..."
 	exit
 fi
-wotprefix="${lutrisPath}world-of-tanks/"
+prefix_name="world-of-tanks"
+#prefix_name="world-of-warships"
+wotprefix="${lutrisPath}${prefix_name}/"
 wotfolder="${wotprefix}drive_c"
-wineversion="$(grep -h -m1 "version:" "$HOME"/.config/lutris/games/world-of-tanks*.yml | head -n1 | sed "s/\s\sversion: //g" -)"
+wineversion="$(grep -h -m1 "version:" "${HOME}/.config/lutris/games/${prefix_name}*.yml" | head -n1 | sed "s/\s\sversion: //g" -)"
+systemwine="false"
+#systemwine="true"
 if [[ "$wineversion" = "" || "$(echo "${wineversion}" | sed "s/lutris-[0-9]\.[0-9]\(\|-[0-9]\(\|[0-9]\)\)-x86_64//g" -)" != "" ]] ; then
 	echo "problem detecting wine version"
-	wineversion="lutris-5.6-2-x86_64"
+	wineversion="lutris-5.7-11-x86_64"
 fi
 winefolder="${HOME}/.local/share/lutris/runners/wine/${wineversion}/"
-if [ -f "${winefolder}dist/bin/wine" ] ; then
+if [ "${systemwine}" = "true" ] ; then
+	wineexec="wine"
+elif [ -f "${winefolder}dist/bin/wine" ] ; then
 	# define wine executable for proton wine
 	wineexec="${winefolder}dist/bin/wine"
 elif [ -f "${winefolder}bin/wine" ] ; then
@@ -28,7 +34,8 @@ else
 fi
 
 if [ ! -d "${wotfolder}" ] ; then
-	wotfolder="$(grep -h -m1 "prefix" "${HOME}"/.config/lutris/games/world-of-tanks*.yml | head -n1 | sed "s/\s\sprefix: //" -)"
+	#wotfolder="$(grep -h -m1 "prefix" "${HOME}"/.config/lutris/games/world-of-tanks*.yml | head -n1 | sed "s/\s\sprefix: //" -)"
+	wotfolder="$(grep -h -m1 "prefix" "${HOME}"/.config/lutris/games/world-of-warships*.yml | head -n1 | sed "s/\s\sprefix: //" -)"
 	if [ ! -d "${wotfolder}" ] ; then
 		echo "problem detecting world of tanks folder, aborting"
 		exit
