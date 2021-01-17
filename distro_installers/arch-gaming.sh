c81e3687658a29b54e61f3e123ac72bcc548db46
Webdesign Höhne <info@webdesign-hoehne.de>
2021-01-17 16:14:55 +0100

    using wine instead of wine-staging for now. newest 6.0 releases cause trouble in some games

--- distro_installers/arch-gaming.sh
+++ distro_installers/arch-gaming.sh
@@ -206,13 +206,14 @@ else
 fi
 
 # if wine is installed, remove it to avoid conflict with wine-staging
-if [[ "$(pacman -Q wine | wc -l)" = "1" && "$(pacman -Q wine | grep wine-staging -c)" = "0" ]] ; then
-	echo "### detected that wine is installed, we need to remove the wine package to install wine-staging"
+if [[ "$(pacman -Q wine-staging | wc -l)" = "1" && "$(pacman -Q wine-staging | grep wine-staging -c)" = "1" ]] ; then
+	#echo "### detected that wine is installed, we need to remove the wine package to install wine-staging"
+	echo "### detected that wine-staging is installed, since it causes some trouble with some games, we roll back to normal wine"
 	echo "### sadly we also need to remove packages depending on wine to avoid conflicts later on"
-	pacman -Rc wine ${installer_addition}
+	pacman -Rc wine-staging ${installer_addition}
 fi
 # different libraries and wine-staging (have to be installed before winetricks)
-pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader --needed ${installer_addition}
+pacman -S wine giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader --needed ${installer_addition}
 
 # dependencies for a lot of games
 pacman -S wine-gecko wine-mono lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse vkd3d lib32-vkd3d lib32-libvdpau libvdpau lib32-libxml2 lib32-sdl2 lib32-freetype2 lib32-dbus sdl_image sdl_mixer --needed ${installer_addition}
