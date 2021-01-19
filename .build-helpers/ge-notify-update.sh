699b62ecbe8814e594f90eac56a2cd9bd2cda0be
Webdesign Höhne <info@webdesign-hoehne.de>
2021-01-19 03:09:37 +0100

    fix if statement to only notify when a valid response is received (bug when disconnected)

--- .build-helpers/ge-notify-update.sh
+++ .build-helpers/ge-notify-update.sh
@@ -6,7 +6,9 @@
 scriptdir="$(dirname "$0")"
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${UID}/bus 
 checkproton="$("$scriptdir"/ge-proton-shaupdate.sh check)"
-if [[ "" != "${checkproton}" && "Latest Release: 6.0-GE-1" = "${checkproton}" ]] ; then
+if [[ "" = "${checkproton}" ]] ; then
+	echo "Version number not found"
+elif [[ "Latest Release: 6.0-GE-1" = "${checkproton}" ]] ; then
 	echo "Version unchanged"
 else
 	echo "Version changed"
