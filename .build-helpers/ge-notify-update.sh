e304817af14d3043b855ec632345af014e15a87b
Andreas Höhne <info@webdesign-hoehne.de>
2021-10-17 19:50:17 +0200

    ge-proton update to 6.19-GE-2

--- .build-helpers/ge-notify-update.sh
+++ .build-helpers/ge-notify-update.sh
@@ -8,7 +8,7 @@ export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${UID}/bus
 checkproton="$("$scriptdir"/ge-proton-shaupdate.sh check)"
 if [[ "" = "${checkproton}" ]] ; then
 	echo "Version number not found"
-elif [[ "Latest Release: 6.16-GE-1" = "${checkproton}" ]] ; then
+elif [[ "Latest Release: 6.19-GE-2" = "${checkproton}" ]] ; then
 	echo "Version unchanged"
 else
 	echo "Version changed"
