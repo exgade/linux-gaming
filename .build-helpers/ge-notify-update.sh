6b13323033c93deee2b34ab87f842bc2a451d96c
Andreas Höhne <info@webdesign-hoehne.de>
2021-12-27 16:11:13 +0100

    update glorious eggroll's proton to 7.0rc2-GE-1

--- .build-helpers/ge-notify-update.sh
+++ .build-helpers/ge-notify-update.sh
@@ -8,7 +8,7 @@ export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${UID}/bus
 checkproton="$("$scriptdir"/ge-proton-shaupdate.sh check)"
 if [[ "" = "${checkproton}" ]] ; then
 	echo "Version number not found"
-elif [[ "Latest Release: 6.21-GE-2" = "${checkproton}" ]] ; then
+elif [[ "Latest Release: 7.0rc2-GE-1" = "${checkproton}" ]] ; then
 	echo "Version unchanged"
 else
 	echo "Version changed"
--- ge-proton.sh
+++ ge-proton.sh
@@ -14,13 +14,13 @@ fi
 
 
 if [ "$1" = "last" ] ; then
-	gerelease="6.20-GE-1"
-	getag="${gerelease}"
-	gechecksum="abdc37adf8b9fe460e99f16050591885b74cbe0884442fff81431c53e63fc07c"
-elif [[ "$1" = "" || "$1" = "both" || "$1" = "latest" ]] ; then
 	gerelease="6.21-GE-2"
 	getag="${gerelease}"
 	gechecksum="98dea10021ae1b2a3ece490034ef5880cb4f778b473ef1b33ad842a39d8c831f"
+elif [[ "$1" = "" || "$1" = "both" || "$1" = "latest" ]] ; then
+	gerelease="7.0rc2-GE-1"
+	getag="${gerelease}"
+	gechecksum="22ccc1a572bf5742696916c620f7c2c68dc35f7c57d31995cbcfbc2c76205b68"
 #elif [[ "$1" = "dev" ]] ; then
 #	gerelease="5.9-GE-2-MF"
 #	getag="${gerelease}"
@@ -46,7 +46,7 @@ elif [[ "$1" = "--cleanup" ]] ; then
 	for tmpdir in Proton-5.{1,2,3,4,5,6,7,8,9}{,1,2,3,4,5,6,7,8,9,0}-GE-{1,2,3,4,5,6,7,8,9}{,-ST,-MF} ; do
 		delete_proton "${tmpdir}"
 	done
-	oldversions="Proton-6.0-GE-1 Proton-6.1-GE-1 Proton-6.1-GE-2 Proton-6.4-GE-1 Proton-6.5-GE-1 Proton-6.5-GE-2 Proton-6.9-GE-2-github-actions-test Proton-6.10-GE-1 Proton-6.12-GE-1 Proton-6.13-GE-1 Proton-6.14-GE-2 Proton-6.15-GE-2 Proton-6.16-GE-1 Proton-6.19-GE-2"
+	oldversions="Proton-6.0-GE-1 Proton-6.1-GE-1 Proton-6.1-GE-2 Proton-6.4-GE-1 Proton-6.5-GE-1 Proton-6.5-GE-2 Proton-6.9-GE-2-github-actions-test Proton-6.10-GE-1 Proton-6.12-GE-1 Proton-6.13-GE-1 Proton-6.14-GE-2 Proton-6.15-GE-2 Proton-6.16-GE-1 Proton-6.19-GE-2 Proton-6.20-GE-1"
 	for tmpdir in $oldversions ; do
 		delete_proton "${tmpdir}"
 	done
