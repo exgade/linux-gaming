c6c960eaf39faeee48bd5fe205e1b6ed16cd4899
Andreas Höhne <info@webdesign-hoehne.de>
2022-06-28 17:03:24 +0200

    patch of ge-proton to 7-23

--- .build-helpers/ge-notify-update.sh
+++ .build-helpers/ge-notify-update.sh
@@ -8,7 +8,7 @@ export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${UID}/bus
 checkproton="$("$scriptdir"/ge-proton-shaupdate.sh check)"
 if [[ "" = "${checkproton}" ]] ; then
 	echo "Version number not found"
-elif [[ "Latest Release: 7-20" = "${checkproton}" ]] ; then
+elif [[ "Latest Release: 7-23" = "${checkproton}" ]] ; then
 	echo "Version unchanged"
 else
 	echo "Version changed"
